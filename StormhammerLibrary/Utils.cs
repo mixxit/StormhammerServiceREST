d0455dab104938011448ee4658fde5c674d579b5
Michael Bolland <{ID}+{username}@users.noreply.github.com>
2021-02-03 21:58:54 +0000

    plumbing

---
+++ StormhammerLibrary/Utils.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StormhammerLibrary
+{
+    public class Utils
+    {
+        public static T Convert<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
